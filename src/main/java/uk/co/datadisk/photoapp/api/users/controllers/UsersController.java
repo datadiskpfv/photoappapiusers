5c726b7cc3ddb2e5ab00332c188d4e58601cbd6f
Paul Valle <paul.valle@datadisk.co.uk>
2019-10-14 20:09:32 +0100

    looking at load balancing

--- src/main/java/uk/co/datadisk/photoapp/api/users/controllers/UsersController.java
+++ src/main/java/uk/co/datadisk/photoapp/api/users/controllers/UsersController.java
@@ -1,5 +1,6 @@
 package uk.co.datadisk.photoapp.api.users.controllers;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UsersController {
 
+  private Environment env;
+
+  public UsersController(Environment env) {
+    this.env = env;
+  }
+
   @GetMapping("/status/check")
   public String status() {
-    return "Working Users Service....";
+    return "Working Users Service, using port " + env.getProperty("local.server.port");
   }
 }
