c884c3b62aec9a85a66eda05a2d049cedbd33492
Paul Valle <paul.valle@datadisk.co.uk>
2019-10-28 13:20:52 +0000

    wrote a decode handler for feign exceptions

--- src/main/java/uk/co/datadisk/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ src/main/java/uk/co/datadisk/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
+import uk.co.datadisk.photoapp.api.users.shared.FeignErrorDecoder;
 
 @SpringBootApplication
 @EnableDiscoveryClient
