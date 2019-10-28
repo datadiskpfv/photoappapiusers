881f8c1add67df89c625cad2c4a88d42b12360f3
Paul Valle <paul.valle@datadisk.co.uk>
2019-10-28 13:29:29 +0000

    installed hystrix and created a album microservice fallback class

--- src/main/java/uk/co/datadisk/photoapp/api/users/data/AlbumsServiceClient.java
+++ src/main/java/uk/co/datadisk/photoapp/api/users/data/AlbumsServiceClient.java
@@ -1,15 +1,28 @@
 package uk.co.datadisk.photoapp.api.users.data;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import uk.co.datadisk.photoapp.api.users.model.AlbumResponseModel;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@FeignClient(name="albums-ws")
+@FeignClient(name="albums-ws", fallback = AlbumsFallback.class)
 public interface AlbumsServiceClient {
 
   @GetMapping("/users/{id}/albums")
   public List<AlbumResponseModel> getAlbums(@PathVariable String id);
+}
+
+// If the album microservice is down then this fallback class will be
+// called and an empty array list is returned
+@Component
+class AlbumsFallback implements AlbumsServiceClient {
+
+  @Override
+  public List<AlbumResponseModel> getAlbums(@PathVariable String id) {
+    return new ArrayList<>();
+  }
 }
